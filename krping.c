larrystevenwise/krping

commit 9fcb9eb9711e9cc74e39c4bec337f226f8802ca6

Include correct header file for signal_pending()
Signed-off-by: Steve Wise <swise@opengridcomputing.com>

krping.c
@@ -46,7 +46,7 @@
 #include <linux/pci.h>
 #include <linux/time.h>
 #include <linux/random.h>
-#include <linux/sched.h>
+#include <linux/sched/signal.h>
 #include <linux/proc_fs.h>
 
 #include <asm/atomic.h>

commit 1eaa91321cd98b4d079632bebbf8cf15484f6f00

Include linux/signal.h
I'm not sure what kernel exported include/linux/sched/signal.h, but it
seems linux/signal.h is the correct include.

Fixes: 9fcb9eb9711e ("Include correct header file for signal_pending()")
Signed-off-by: Steve Wise <swise@opengridcomputing.com>

krping.c
@@ -46,7 +46,7 @@
 #include <linux/pci.h>
 #include <linux/time.h>
 #include <linux/random.h>
-#include <linux/sched/signal.h>
+#include <linux/signal.h>
 #include <linux/proc_fs.h>
 
 #include <asm/atomic.h>